bcf6ca5be44562b1226d750fe13e59555277ec5b
MKhozinA <m.khozin.ft19@mail.umy.ac.id>
2022-01-10 07:41:25 +0700

    Add project files.

---
+++ WCFService_2Way_123/IClientCallback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace WCFService_2Way_123
+{
+    [ServiceContract]
+    interface IClientCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void pesanKirim(string user, string pesan);
+    }
+}
